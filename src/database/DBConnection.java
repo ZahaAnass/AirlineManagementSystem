ad63fa3d479b3d4e2249a6dd49b02dc578bf71f9
ZahaAnass <zahaanass277@gmail.com>
2025-11-24 20:43:27 +0100

    add database connection

---
+++ src/database/DBConnection.java
@@ -0,0 +1,13 @@
+package database;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3308/airline_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
